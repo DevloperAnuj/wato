cf205b500ae6addaf30c7683f749109fb69b1f59
DevloperAnuj <dev.anujkamble+github.com>
2023-10-29 16:23:32 +0530

    Initialize Firebase  Analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:easy_splash_screen/easy_splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:wato/config/service_locator.dart';
 
 import 'features/whatsapp_direct/presentation/pages/home_page/home_page.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await MobileAds.instance.initialize();
   initSetup();
   runApp(const MyApp());
